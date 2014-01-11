d48b96dec92ed7486e2f6e3f32a79196532a8f6c
joneshf <jones3.hardy@gmail.com>
2014-01-11 03:59:06 -0800

    Interesting.

--- improving_on_saddleback_search/invert.hs
+++ improving_on_saddleback_search/invert.hs
@@ -39,6 +39,27 @@ findT n (u, v) f z
     | z' > z            =        findT n (u,     v - 1) f z
         where z' = f (u, v)
 
+findM :: (Int, Int) -> (Int, Int) -> ((Int, Int) -> Int) -> Int -> [(Int, Int)]
+findM (u, v) (r, s) f z
+    | u > r || v < s = []
+    | v - s <= r - u = rfind (bsearchT (\x -> f (x, q)) (u - 1, r + 1) z)
+    | otherwise      = cfind (bsearchT (\y -> f (p, y)) (s - 1, v + 1) z)
+        where p = (u + r) `div` 2
+              q = (v + s) `div` 2
+              rfind p = (if f (p, q) == z
+                         then (p, q):findM (u, v) (p - 1, q + 1) f z
+                         else        findM (u, v) (p,     q + 1) f z
+                        ) ++ findM (p + 1, q - 1) (r, s) f z
+              cfind q = findM (u, v) (p - 1, q + 1) f z ++
+                        (if f (p, q) == z
+                         then (p, q):findM (p + 1, q - 1) (r, s) f z
+                         else        findM (p + 1, q)     (r, s) f z)
+
+invertM :: ((Int, Int) -> Int) -> Int -> [(Int, Int)]
+invertM f z = findM (0, m) (n, 0) f z
+    where m = bsearchT (\y -> f (0, y)) (-1, z + 1) z
+          n = bsearchT (\x -> f (x, 0)) (-1, z + 1) z
+
 f0 (x, y) = 2^y * (2*x + 1) - 1
 f1 (x, y) = x*2^x + y*2^y + 2*x + y
 f2 (x, y) = 3*x + 27*y + y^2
