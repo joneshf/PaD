0822f4b33daf8c838739c8325fd81d87c950736f
joneshf <jones3.hardy@gmail.com>
2014-01-15 21:09:39 -0800

    Still kind of confusing.

--- 6_making_a_century/century.hs
+++ 6_making_a_century/century.hs
@@ -32,3 +32,36 @@ glue x ((xs:xss):xsss) = [ ((x:xs):xss):xsss
                          , ([x]:xs:xss):xsss
                          , [[x]]:(xs:xss):xsss
                          ]
+
+value :: Expression -> (Int, Int, Int, Int)
+value ((xs:xss):xsss) = (10 ^ n, valFact xs, valTerm xss, valExpr xsss)
+    where n = length xs
+
+modify :: Int -> (Int, Int, Int, Int) -> [(Int, Int, Int, Int)]
+modify x (k, f, t, e) = [ (10 * k, k * x + f, t, e)
+                        , (10, x, f * t, e)
+                        , (10, x, 1, f * t + e)
+                        ]
+
+good', ok :: Int -> (Int, Int, Int, Int) -> Bool
+good' c (_, f, t, e) = f * t + e == c
+ok    c (_, f, t, e) = f * t + e <= c
+
+solutions :: Digit -> Factor -> [Expression]
+solutions c = map fst . filter (good' c . snd) . foldr (expand c) []
+
+expand :: Digit
+       -> Digit
+       -> [(Expression, (Digit, Digit, Digit, Digit))]
+       -> [(Expression, (Digit, Digit, Digit, Digit))]
+expand _ x []  = [([[[x]]], (10, x, 1, 0))]
+expand c x evs = concatMap (filter (ok c . snd) . glue' x) evs
+
+glue' :: Digit
+      -> (Expression, (Digit, Digit, Digit, Digit))
+      -> [(Expression, (Digit, Digit, Digit, Digit))]
+glue' x ((xs:xss):xsss, (k, f, t, e)) =
+    [ (((x:xs):xss):xsss, (10 * k, k * x + f, t, e))
+    , (([x]:xs:xss):xsss, (10, x, f * t, e))
+    , ([[x]]:(xs:xss):xsss, (10, x, 1, f * t + e))
+    ]
