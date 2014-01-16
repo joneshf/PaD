4fc74d376fee40f92a996f578b54e3ea8abbd986
joneshf <jones3.hardy@gmail.com>
2014-01-15 20:48:41 -0800

    Continuing this pearl.

--- 6_making_a_century/century.hs
+++ 6_making_a_century/century.hs
@@ -16,3 +16,19 @@ valFact = foldl1 (\n d -> 10 * n + d)
 
 good :: Int -> Bool
 good = (== 100)
+
+expressions :: Factor -> [Expression]
+-- This would work if `paritions` where somewhere to be found.
+--expressions = concatMap partitions . partitions
+expressions = foldr extend []
+
+extend :: Digit -> [Expression] -> [Expression]
+extend x [] = [[[[x]]]]
+extend x es = concatMap (glue x) es
+
+-- Don't fully grok tis one.  Might be still too sick...
+glue :: Digit -> Expression -> [Expression]
+glue x ((xs:xss):xsss) = [ ((x:xs):xss):xsss
+                         , ([x]:xs:xss):xsss
+                         , [[x]]:(xs:xss):xsss
+                         ]
